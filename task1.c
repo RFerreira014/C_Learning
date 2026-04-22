6a1715e8aad64fa8a06c750d1e2afeba9048c850
Rui Ferreira <speranikta@hotmail.com>
2026-04-22 19:53:46 +0100

    task 1: estrutura basica e variaveis

---
+++ task1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    char name = 'R';
+    int year = 1975;
+    float gpa = 18;
+    printf(" Name %c\n", name);
+    printf("Year %d\n", year);
+    printf("gpa  %.1f\n", gpa);
+    return 0;
+
+}
